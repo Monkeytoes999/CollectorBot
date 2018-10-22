290c7a52f1ff05d27a9f56f555346404f95b499a
Monkeytoes999 <32685880+Monkeytoes999@users.noreply.github.com>
2018-10-22 17:58:52 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,5 @@
 var Discord = require('discord.io');
 var logger = require('winston');
-var auth = require('./auth.json');
 var fs = require('fs');
 var user_data = require('./user_data.json');
 
@@ -12,7 +11,7 @@ logger.add(new logger.transports.Console, {
 logger.level = 'debug';
 // Initialize Discord Bot
 var bot = new Discord.Client({
-   token: auth.token,
+   token: process.env.token,
    autorun: true
 });
 
