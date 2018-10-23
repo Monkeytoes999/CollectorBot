a347b4fbbbab299c3ee08e505e6bd81c498d5f78
Monkeytoes999 <32685880+Monkeytoes999@users.noreply.github.com>
2018-10-22 20:00:28 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -29,7 +29,6 @@ client.query('SELECT table_schema,table_name FROM information_schema.tables;', (
   for (let row of res.rows) {
     console.log(JSON.stringify(row));
   }
-  client.end();
 });
 
 bot.on('ready', function (evt) {
