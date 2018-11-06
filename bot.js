db43f2aadcababc83544f2afbe62dfa2ed0df32b
Monkeytoes999 <32685880+Monkeytoes999@users.noreply.github.com>
2018-11-05 19:22:01 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,13 +1,6 @@
 var Discord = require('discord.io');
 var logger = require('winston');
 var fs = require('fs');
-var user_data = require('./user_data.json');
-var usertest = {money: 0, karma: 0, pets: [0, 0, 0], totalPets: [0, 0, 0]};
-var config = {
-    databaseURL: "https://discordbot-a817a.firebaseio.com"
-  };
-  firebase.initializeApp(config);
-var database = firebase.database();
 
 
 // Configure logger settings
@@ -37,12 +30,6 @@ bot.on('message', function (user, userID, channelID, message, evt) {
 	
 	message = message.toUpperCase();
 	
-	if (evt.d.mentions.length > 0 && evt.d.mentions[0].id == "393586279964475393"){
-  		bot.sendMessage({
-        		to: channelID,
-     			message: 'hello'
-    		});
-	}
 	
     if (message.substring(0, 1) == '?') {
         var args = message.substring(1).split(' ');
@@ -58,26 +45,31 @@ bot.on('message', function (user, userID, channelID, message, evt) {
                     message: 'Pong!'
                 });
             break;
+		case 'NEW_USER':
+			bot.sendMessage({
+				to: '509149632618823681',
+				message: 0, 0, 0, 0, 0, 0, 0, 0
+			});
+			break;
+		case 'dothis':
+			bot.sendMessage({
+				to: '509160162959949825',
+				message: message.substring(8)
+			});
 			case 'BEG':
 				if (user_data[userID] != undefined){
 						bot.sendMessage({
 							to: channelID,
 							message: user + ', your begging has been answered. Your lead count has increased by 1'
 						});
-					user_data[userID]['money'] = user_data[userID]['money'] + 1;
-
-					fs.writeFile('./user_data.json', JSON.stringify(user_data, null, 4), (err) => {
-						// good to go if no err(or)
 					});
 				}
 				break;
 				case 'LEAD':
-					if (user_data[userID] != undefined){
 						bot.sendMessage({
 							to: channelID,
-							message: user + ', you currently have ' + user_data[userID]['money'] + ' lead!'
+							message: user + ', you currently have ' +  ' lead!'
 						});
-					}
 					break;
 			case 'ORANGE':
 				bot.sendMessage({
