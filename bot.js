6e55c8ab59bec8d32b9fb487fff4c1dcacd569ed
Monkeytoes999 <32685880+Monkeytoes999@users.noreply.github.com>
2018-11-09 17:49:57 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -162,7 +162,7 @@ bot.on('message', function (user, userID, channelID, message, evt) {
 							userrcID = 9;
 							subofGive = 29;
 						}
-						if (tacobell.content.includes(message.substring(userrcID, userrcID + 18)) && message.substring(8, 26) != userID){
+						if (tacobell.content.includes(message.substring(userrcID, userrcID + 18)) && message.substring(userrcID, userrcID) != userID){
 							let giverMessID = (tacobell.content.substring((tacobell.content.indexOf(userID) + 20), (tacobell.content.indexOf(userID) + 38)));
 							let recieverMessID = (tacobell.content.substring((tacobell.content.indexOf(message.substring(userrcID, userrcID + 18)) + 20), (tacobell.content.indexOf(message.substring(userrcID, userrcID + 18)) + 38)));
 							bot.getMessage({
