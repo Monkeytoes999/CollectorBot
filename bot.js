314a5d95a71bb6cdf89bb46d8be5f921f89e2db2
Monkeytoes999 <32685880+Monkeytoes999@users.noreply.github.com>
2019-02-11 19:34:07 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -176,13 +176,20 @@ bot.on('message', function (user, userID, channelID, message, evt) {
         switch(cmd) {
             // !ping
 		case 'TEST':
-			pool.query('SELECT * FROM users ORDER BY id ASC', (error, results) => {
+			const getUserById = (request, response) => {
+			  const id = parseInt(request.params.id)
+
+			  pool.query('SELECT * FROM users WHERE id = $1', [id], (error, results) => {
 			    if (error) {
 			      throw error
 			    }
 			    response.status(200).json(results.rows)
 			  })
 			}
+			bot.sendMessage({
+				to: channelID,
+				message: getUserById
+			});
 			break;
 		case 'PING':
 			bot.sendMessage({
